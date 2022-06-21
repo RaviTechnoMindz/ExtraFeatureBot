30b9317182ceda38e17ee36400b4c3371fb7e927
Techno Mindz <97782515+TechnoMindz@users.noreply.github.com>
2022-06-21 12:01:11 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('updates', url='https:/t.me/justaupdatechannel')
+            InlineKeyboardButton('help', url=f"https://t.me/technomindzchat"),
+            InlineKeyboardButton('updates', url='https:/t.me/tmmainchannel')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -47,7 +47,7 @@ async def save_group(bot, message):
     else:
         for u in message.new_chat_members:
             zaute = [[
-            InlineKeyboardButton('👉 🎥 Press me... 🎥 👈', url="https://t.me/cinemaenglish123")
+            InlineKeyboardButton('👉 🎥 Press me... 🎥 👈', url="https://t.me/technomoviescollection")
         ]]
             if (temp.MELCOW).get('welcome') is not None:
                 try:
